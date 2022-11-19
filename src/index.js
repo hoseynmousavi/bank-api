7d017343ce4226664c2df7f7b2bf70e0771c156c
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-11-19 13:31:43 +0330

    initial

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import cors from "cors"
 import fileUpload from "express-fileupload"
 import mongoose from "mongoose"
 import data from "./data"
-import notFoundRouter from "./routes/notFoundRouter"
 import bankRouter from "./routes/bankRouter"
-
+import fileRouter from "./routes/fileRouter"
+import notFoundRouter from "./routes/notFoundRouter"
 
 const app = express()
 app.use(cors())
@@ -17,6 +17,7 @@ mongoose.Promise = global.Promise
 mongoose.connect(data.connectServerDb, null, () => console.log("connected to db"))
 
 bankRouter(app)
+fileRouter(app, __dirname)
 notFoundRouter(app)
 
 app.listen(data.port, () => console.log(`server is Now Running on Port ${data.port}`))
