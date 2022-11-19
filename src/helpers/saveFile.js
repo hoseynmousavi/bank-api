37a35b998a87ebe4130464930330e11dd74c17b1
Hoseyn <hoseyn.mousavi78@gmail.com>
2022-11-19 13:12:10 +0330

    initial

--- src/helpers/saveFile.js
+++ src/helpers/saveFile.js
@@ -13,6 +13,7 @@ function saveFile({file, res})
             {
                 if (err)
                 {
+                    console.log(err)
                     createErrorText({res, status: 400, message: respondTextConstant.error.fileSave, detail: err})
                 }
                 else
